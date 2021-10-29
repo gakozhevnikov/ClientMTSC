8441eb010b6d2e5285975826ba5787aad0545230
gakozhevnikov <geo9@yandex.ru>
2021-10-29 17:10:16 +0500

    Edit user.service.ts

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 
-const USER_URL='http://localhost:8080/user/'
+const USER_URL= 'http://localhost:8080/user/'
 
 @Injectable({
   providedIn: 'root'
